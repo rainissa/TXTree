64c3e25601fd4e300443d2b210947704bf633327
ashila-AS <ashila.aulia.tif25@polban.ac.id>
2026-03-16 23:58:10 +0700

    memperbaiki header fitur cut copy dan paste

--- clipboard.h
+++ clipboard.h
@@ -4,14 +4,17 @@
 #define MAX_BARIS 100
 #define MAX_KARAKTER 256
 
-/* variabel global dari dokumen */
+/* variabel global dokumen */
 extern char dokumen[MAX_BARIS][MAX_KARAKTER];
 extern char clipboard[MAX_KARAKTER];
 extern int jumlahBaris;
 extern int cursor;
 
-/* fungsi pengaturan cursor */
+/* pengaturan cursor */
 void setCursor(int posisi);
+
+/* input teks dengan auto pindah baris */
+void inputText(char teks[]);
 
 /* operasi clipboard */
 void copyLine();
