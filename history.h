c6f381ffba6af45f0dfbb7b671a1e59055c9a526
shiningsstars <hanjjeongiee@gmail.com>
2026-03-17 17:47:08 +0700

    fix header undo-redo

--- history.h
+++ history.h
@@ -5,13 +5,17 @@
 
 #define STACK_SIZE 100
 
-typedef struct{
-    int row;
-    int karakter;
-    char prev_char;
+// Enumerasi untuk membedakan tipe aksi
+typedef enum { EDIT, TAMBAH, HAPUS } ActionType;
+
+typedef struct {
+    int row;                      // Posisi baris di Array 2D
+    char dataLama[MAX_KARAKTER];  // Teks lama sebelum diubah
+    ActionType tipe;              // Jenis aksi yang dilakukan
 } Action;
 
-void pushUndo(int row, int karakter, char c);
+// Fungsi utama History
+void pushUndo(int row, char *teks, ActionType tipe);
 void undo();
 void redo();
 void clearRedo();
