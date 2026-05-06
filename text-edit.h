78c7c74dd6707417f2e946e2e702654e1d7988b9
rainissa <rainissaazizah@gmail.com>
2026-05-06 19:28:15 +0700

    Refactor text-edit ke linked list, tambah insertFirst/Last/Before

--- text-edit.h
+++ text-edit.h
@@ -2,15 +2,33 @@
 #define TEXT_EDIT_H
 
 #include "config.h"
+#include <stdlib.h>
+#include <string.h>
+#define Nil NULL
+#define Info(P) (P)->teks
+#define Next(P) (P)->next
+#define Prev(P) (P)->prev
+#define First(L) (L).first
 
-extern char buffer[MAX_ROW][MAX_KARAKTER];
-extern int jumlahBaris;
+typedef char infotype[MAX_KARAKTER];
+typedef struct BarisTeks *address;
+typedef struct BarisTeks{
+    infotype teks;
+    address next;
+    address prev;
+} BarisTeks;
+typedef struct {
+    address first;
+} List;
 
+void CreateList (List *L);
 void pauseScreen();
+void insertFirst(address P);
+void insertLast(address P);
+void insertBefore(address P, address temp);
 void tampilkan();
-
 void tambahBaris();
 void editBaris();
-void sisipBaris();
+address Alokasi(infotype x);
 
 #endif
